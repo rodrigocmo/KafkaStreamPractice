92f7a8a1aa6d3cad0f483b9378c95670ccdaeec8
Rodrigo Morais <rcastromorais01@gmail.com>
2023-11-25 16:05:49 -0300

    Start project

---
+++ src/main/java/com/example/kafkastreampractice/KafkaStreamPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkastreampractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStreamPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kafkastreampractice/KafkaStreamPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkastreampractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
